75664926049cc41f05690c289fd5f89b64459026
daniel2010001 <daniel.boris.limachi.cuiza@gmail.com>
2024-11-13 07:25:21 -0400

    Add refactor request

--- apps/ckan/views.py
+++ apps/ckan/views.py
@@ -3,18 +3,27 @@ from apps.users.models import User
 from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.views import APIView
-from rest_framework import permissions, status
+from rest_framework import permissions
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_502_BAD_GATEWAY
+from rest_framework.views import APIView
 from urllib.parse import ParseResult
-import json
 import requests
 
 
 @method_decorator(csrf_exempt, name="dispatch")
 class CkanApiView(APIView):
     permission_classes = [permissions.AllowAny]
+
+    def ckan_error_response(
+        self, message: str, status_code: int = HTTP_400_BAD_REQUEST
+    ):
+        error_response = {
+            "success": False,
+            "error": {"message": message, "__type": "CKANError"},
+        }
+        return Response(error_response, status=status_code)
 
     def get_ckan_url(self, ckan_service: str, query_params=None):
         ckan_url: ParseResult = settings.CKAN_URL
@@ -25,120 +34,55 @@ class CkanApiView(APIView):
 
     def get_headers(self, request: Request):
         headers = {"Content-Type": "application/json"}
-        if request.user.is_authenticated:
-            user_profile = User.objects.get(name=request.user.name)
-            if user_profile and user_profile.token:
-                headers["Authorization"] = user_profile.token
+        if request.user and request.user.is_authenticated:
+            user = User.objects.get(name=request.user.name)
+            if user and user.token:
+                headers["Authorization"] = user.token
         return headers
 
     def get_data(self, request: Request):
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError:
-            return Response(
-                data={
-                    "error": {"__type": "Not Found Error", "message": "Invalid JSON"},
-                    "success": False,
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        data = request.data
+        if not data:
+            return self.ckan_error_response(message="Invalid JSON")
         return data
 
-    def get(self, request: Request, ckan_service: str):
-        query_params = request.META["QUERY_STRING"]
+    def query(self, request: Request, ckan_service: str):
+        query_params = request.META.get("QUERY_STRING", "")
         url = self.get_ckan_url(ckan_service, query_params)
         headers = self.get_headers(request)
+        data = self.get_data(request)
         try:
-            response = requests.get(url=url, headers=headers)
+            match request.method:
+                case "GET":
+                    response = requests.get(url=url, headers=headers)
+                case "POST":
+                    response = requests.post(url=url, headers=headers, json=data)
+                case "PUT":
+                    response = requests.put(url=url, headers=headers, json=data)
+                case "DELETE":
+                    response = requests.delete(url=url, headers=headers)
+                case _:
+                    return self.ckan_error_response("Invalid HTTP Method")
         except requests.exceptions.ConnectionError:
-            return Response(
-                data={
-                    "error": {
-                        "__type": "Not Found Error",
-                        "message": "Connection Error",
-                    },
-                    "success": False,
-                },
-                status=status.HTTP_502_BAD_GATEWAY,
+            return self.ckan_error_response(
+                message="Connection Error", status_code=HTTP_502_BAD_GATEWAY
             )
         try:
             data = filter_sensitive_data(response.json(), ["help"])
         except ValueError:
-            return Response(
-                {"error": "Invalid JSON"}, status=status.HTTP_502_BAD_GATEWAY
+            return self.ckan_error_response(
+                message="Invalid JSON", status_code=HTTP_502_BAD_GATEWAY
             )
         return Response(data=data, status=response.status_code)
+
+    def get(self, request: Request, ckan_service: str):
+        return self.query(request, ckan_service)
 
     def post(self, request: Request, ckan_service: str):
-        url = self.get_ckan_url(ckan_service)
-        headers = self.get_headers(request)
-        json = self.get_data(request)
-        try:
-            response = requests.post(url=url, headers=headers, json=json)
-        except requests.exceptions.ConnectionError:
-            return Response(
-                data={
-                    "error": {
-                        "__type": "Not Found Error",
-                        "message": "Connection Error",
-                    },
-                    "success": False,
-                },
-                status=status.HTTP_502_BAD_GATEWAY,
-            )
-        try:
-            data = filter_sensitive_data(response.json(), ["help"])
-        except ValueError:
-            return Response(
-                {"error": "Invalid JSON"}, status=status.HTTP_502_BAD_GATEWAY
-            )
-        return Response(data=data, status=response.status_code)
+        return self.query(request, ckan_service)
 
     def put(self, request: Request, ckan_service: str):
-        url = self.get_ckan_url(ckan_service)
-        headers = self.get_headers(request)
-        json = self.get_data(request)
-        try:
-            response = requests.put(url=url, headers=headers, json=json)
-        except requests.exceptions.ConnectionError:
-            return Response(
-                data={
-                    "error": {
-                        "__type": "Not Found Error",
-                        "message": "Connection Error",
-                    },
-                    "success": False,
-                },
-                status=status.HTTP_502_BAD_GATEWAY,
-            )
-        try:
-            data = filter_sensitive_data(response.json(), ["help"])
-        except ValueError:
-            return Response(
-                {"error": "Invalid JSON"}, status=status.HTTP_502_BAD_GATEWAY
-            )
-        return Response(data=data, status=response.status_code)
+        return self.query(request, ckan_service)
 
     def delete(self, request: Request, ckan_service: str):
-        url = self.get_ckan_url(ckan_service)
-        headers = self.get_headers(request)
-        try:
-            response = requests.delete(url=url, headers=headers)
-        except requests.exceptions.ConnectionError:
-            return Response(
-                data={
-                    "error": {
-                        "__type": "Not Found Error",
-                        "message": "Connection Error",
-                    },
-                    "success": False,
-                },
-                status=status.HTTP_502_BAD_GATEWAY,
-            )
-        try:
-            data = filter_sensitive_data(response.json(), ["help"])
-        except ValueError:
-            return Response(
-                {"error": "Invalid JSON"}, status=status.HTTP_502_BAD_GATEWAY
-            )
-        return Response(data=data, status=response.status_code)
+        return self.query(request, ckan_service)
